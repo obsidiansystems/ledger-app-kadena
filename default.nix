3e127d80b49547c90303705ec05dd9d7876899a2
John Ericson <John.Ericson@Obsidian.Systems>
2023-03-09 16:40:25 +0000

    Fix macOS build

--- default.nix
+++ default.nix
@@ -1,5 +1,8 @@
+{ localSystem ? { system = builtins.currentSystem; }
+}:
+
 rec {
-  alamgu = import ./dep/alamgu {};
+  alamgu = import ./dep/alamgu { inherit localSystem; };
 
   inherit (alamgu) lib pkgs crate2nix alamguLib;
 
@@ -41,7 +44,9 @@ rec {
       ];
   };
 
-  makeTarSrc = { appExe, device }: pkgs.runCommandCC "make-tar-src-${device}" {
+  makeTarSrc = { appExe, device }:
+  let collection = alamgu.perDevice.${device};
+  in collection.ledgerPkgs.runCommandCC "${appName}-${device}-tar-src" {
     nativeBuildInputs = [
       alamgu.cargo-ledger
       alamgu.ledgerRustPlatform.rust.cargo
@@ -78,7 +83,11 @@ rec {
 
   apiPort = 5005;
 
+  # Tests don't yet run on Darwin
   runTests = { appExe, device, variant ? "", speculosCmd }:
+  if pkgs.stdenv.hostPlatform.isDarwin
+  then null
+  else
   pkgs.runCommandNoCC "run-tests-${device}${variant}" {
     nativeBuildInputs = [
       pkgs.wget alamgu.speculos.speculos testScript
